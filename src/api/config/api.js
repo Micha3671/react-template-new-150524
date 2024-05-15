547389524e5f637c9cec6e299616928a4b2a974e
Micha <michael.rigas@t-online.de>
2024-05-15 15:34:58 +0200

    Change API

--- src/api/config/api.js
+++ src/api/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://zzlaeeske3m7vjsfdxdrrrpg7q0rwirr.lambda-url.eu-central-1.on.aws/v1",
+    "https://bwk77bagpk7jjyoj3zv27j4tmm0pazvx.lambda-url.eu-central-1.on.aws/v1",
 });
 
 export default api;
